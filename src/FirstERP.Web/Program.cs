e6e959a6b08d05dc9d632d8093563dfc69b38f0d
Aswathy.T.S <51845318+AswathyDayal@users.noreply.github.com>
2026-03-03 10:54:17 +0530

    Add sales and product dashboard page

--- src/FirstERP.Web/Program.cs
+++ src/FirstERP.Web/Program.cs
@@ -20,6 +20,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
